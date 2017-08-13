f85aaa66b6360e6fb8237ce8f6e2a865c9187046
Fred Morrison <fpmorrison@verizon.net>
2017-08-13 08:45:59 -0400

    finished bare bones example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component, VERSION } from '@angular/core';
+import {Component, OnInit, VERSION} from '@angular/core';
+
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+
+import {SearchItem} from './search/search-item';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import {SearchService} from './search/search.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = `app running Angular version ${VERSION.full}`;
+export class AppComponent implements OnInit {
+
+	private _loading: boolean = false;
+	get loading(): boolean {
+		return this._loading;
+	}
+
+	set loading(value: boolean) {
+		this._loading = value;
+	}
+
+	private _title: string;
+	get title(): string {
+		return this._title;
+	}
+
+	private _results: Observable<SearchItem[]>;
+	get results(): Observable<SearchItem[]> {
+		return this._results;
+	}
+
+	set results(value: Observable<SearchItem[]>) {
+		this._results = value;
+	}
+
+	private _searchField: FormControl;
+	get searchField(): FormControl {
+		return this._searchField;
+	}
+	set searchField(value: FormControl){
+		this._searchField = value;
+	}
+
+	constructor(private itunes: SearchService) {
+	}
+
+	ngOnInit() {
+		this._title = `${VERSION.full}`;
+		this.searchField = new FormControl();
+		this._results = this.searchField.valueChanges
+			.debounceTime(400)
+			.distinctUntilChanged()
+			.do(_ => this._loading = true)
+			.switchMap(term => this.itunes.search(term))
+			.do(_ => this._loading = false);
+	}
+
+	doSearch(term: string) {
+		this.itunes.search(term);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchService } from './search/search.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SearchService} from './search/search.service';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [SearchService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		FormsModule,
+		HttpModule,
+		AppRoutingModule
+	],
+	providers: [SearchService],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
